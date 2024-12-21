af24f771e116e7639497a4e4ad7e127f8224f803
igachev <ivophoenix1135@gmail.com>
2024-12-21 23:57:16 +0200

    added @Bean in starting file QuestionGameApiApplication
    This bean initializes the USER and ADMIN roles in our database in table role

--- question-game-api/src/main/java/com/example/question_game_api/QuestionGameApiApplication.java
+++ question-game-api/src/main/java/com/example/question_game_api/QuestionGameApiApplication.java
@@ -1,7 +1,12 @@
 package com.example.question_game_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.question_game_api.role.Role;
+import com.example.question_game_api.role.RoleRepository;
 
 @SpringBootApplication
 public class QuestionGameApiApplication {
@@ -9,5 +14,21 @@ public class QuestionGameApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionGameApiApplication.class, args);
 	}
+
+@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return (args) -> {
+			if(roleRepository.findByName("USER").isEmpty()) {
+				var role = new Role();
+				role.setName("USER");
+				roleRepository.save(role);
+			}
+			if(roleRepository.findByName("ADMIN").isEmpty()) {
+				var role = new Role();
+				role.setName("ADMIN");
+				roleRepository.save(role);
+			}
+		};
+	};
 
 }
