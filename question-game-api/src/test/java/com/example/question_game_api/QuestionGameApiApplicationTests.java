35b7ba82902bbd7ae607b6a442cbfdbee1908492
igachev <ivophoenix1135@gmail.com>
2024-12-21 17:38:54 +0200

    I initialized spring boot project
    I added the necessary dependencies

---
+++ question-game-api/src/test/java/com/example/question_game_api/QuestionGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.question_game_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
