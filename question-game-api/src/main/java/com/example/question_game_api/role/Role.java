f6016e48665250a215390c8caa88436adf75d5d6
igachev <ivophoenix1135@gmail.com>
2024-12-21 23:55:23 +0200

    created the User and Role models

---
+++ question-game-api/src/main/java/com/example/question_game_api/role/Role.java
@@ -0,0 +1,65 @@
+package com.example.question_game_api.role;
+
+import java.util.List;
+
+import com.example.question_game_api.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Role {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() { 
+
+    }
+
+    public Role(Integer id, String name, List<User> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", name=" + name + ", users=" + users + "]";
+    }
+
+    
+}
---
+++ question-game-api/src/main/java/com/example/question_game_api/user/User.java
@@ -0,0 +1,146 @@
+package com.example.question_game_api.user;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.question_game_api.role.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails,Principal {
+
+@Id
+@GeneratedValue
+private Integer id;
+private String firstName;
+private String lastName;
+@Column(unique = true)
+private String email;
+private String password;
+@ManyToMany
+private List<Role> roles;
+
+public User() {
+
+}
+
+public User(Integer id, String firstName, String lastName, String email, String password, List<Role> roles) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.roles = roles;
+}
+
+
+
+public Integer getId() {
+    return id;
+}
+
+public void setId(Integer id) {
+    this.id = id;
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public List<Role> getRoles() {
+    return roles;
+}
+
+public void setRoles(List<Role> roles) {
+    this.roles = roles;
+}
+
+@Override
+public String getName() {
+    return email;
+}
+
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles
+    .stream()
+    .map((role) -> 
+    new SimpleGrantedAuthority(role.getName()))
+    .collect(Collectors.toList());
+}
+
+@Override
+public String getPassword() {
+    return password;
+}
+
+@Override
+public String getUsername() {
+    return email;
+}
+
+@Override
+public boolean isAccountNonExpired() {
+   return true;
+}
+
+@Override
+public boolean isAccountNonLocked() {
+    return true;
+}
+
+@Override
+public boolean isCredentialsNonExpired() {
+    return true;
+}
+
+@Override
+public boolean isEnabled() {
+    return true;
+}
+
+@Override
+public String toString() {
+    return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password="
+            + password + ", roles=" + roles + "]";
+}
+
+
+}
