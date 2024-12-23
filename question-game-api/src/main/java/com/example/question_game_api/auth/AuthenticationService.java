f368f7e0676be1c153c576a4d6fc4e5122ebac61
igachev <ivophoenix1135@gmail.com>
2024-12-23 17:12:42 +0200

    updated AuthenticationService
    I implemented the login method

--- question-game-api/src/main/java/com/example/question_game_api/auth/AuthenticationService.java
+++ question-game-api/src/main/java/com/example/question_game_api/auth/AuthenticationService.java
@@ -3,12 +3,15 @@ package com.example.question_game_api.auth;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.question_game_api.exceptions.UserAlreadyExistsException;
 import com.example.question_game_api.role.Role;
 import com.example.question_game_api.role.RoleRepository;
+import com.example.question_game_api.security.JwtService;
 import com.example.question_game_api.user.User;
 import com.example.question_game_api.user.UserRepository;
 
@@ -19,6 +22,8 @@ public class AuthenticationService {
 private PasswordEncoder passwordEncoder;
 private UserRepository userRepository;
 private RoleRepository roleRepository;
+private AuthenticationManager authenticationManager;
+private JwtService jwtService;
 
 public AuthenticationService() { }
 
@@ -27,14 +32,16 @@ public AuthenticationService() { }
     public AuthenticationService(
     PasswordEncoder passwordEncoder, 
     UserRepository userRepository,
-    RoleRepository roleRepository
+    RoleRepository roleRepository,
+    AuthenticationManager authenticationManager,
+    JwtService jwtService
     ) {
     this.passwordEncoder = passwordEncoder;
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
+    this.authenticationManager = authenticationManager;
+    this.jwtService = jwtService;
 }
-
-
 
     public RegisterResponse register(RegisterRequestDto registerRequestDto) throws Exception {
        
@@ -62,4 +69,16 @@ public AuthenticationService() { }
         return registerResponse;
     }
     
+    public LoginResponse login(LoginRequestDto loginRequestDto) {
+        var auth = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword())
+        );
+       // System.out.println(auth);
+        var user = (User) auth.getPrincipal();
+        var token = jwtService.generateToken(user);
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(token);
+        return loginResponse;
+    }
+
 }
