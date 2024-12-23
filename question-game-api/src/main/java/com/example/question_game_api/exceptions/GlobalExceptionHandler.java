4a8cbfaac0b66b6e0b79eb76be2156ad2f1da6c8
igachev <ivophoenix1135@gmail.com>
2024-12-23 15:40:52 +0200

    created GlobalExceptionHandler
    1)I added error handling for MethodArgumentNotValidException
    2)I added error handling for UserAlreadyExistsException

---
+++ question-game-api/src/main/java/com/example/question_game_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.example.question_game_api.exceptions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorObject> handleException(
+        MethodArgumentNotValidException ex,
+        WebRequest webRequest
+    ) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        Set<String> errors = new HashSet<>();
+        ex.getBindingResult().getAllErrors()
+        .forEach((err) -> errors.add(err.getDefaultMessage()));
+
+        errorObject.setErrorList(errors);
+        
+        return ResponseEntity.ok(errorObject);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorObject> handleException(
+        UserAlreadyExistsException ex,
+        WebRequest webRequest
+    ) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+        Set<String> errors = new HashSet<>();
+        errors.add(ex.getMessage());
+        errorObject.setErrorList(errors);
+
+        return ResponseEntity.ok(errorObject);
+    }
+
+}
