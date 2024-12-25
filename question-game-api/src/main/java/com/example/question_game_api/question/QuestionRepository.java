d775dd8912180dacab257b72fe8f7f8130984c42
igachev <ivophoenix1135@gmail.com>
2024-12-25 23:21:20 +0200

    updated QuestionRepository
    created a query to generate unique random questions

--- question-game-api/src/main/java/com/example/question_game_api/question/QuestionRepository.java
+++ question-game-api/src/main/java/com/example/question_game_api/question/QuestionRepository.java
@@ -1,11 +1,23 @@
 package com.example.question_game_api.question;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
     boolean existsByIgnoreCaseQuestion(String question);
+
+    // generates 10 unique random questions
+    @Query(value = """
+        SELECT *
+        FROM question
+        ORDER BY RANDOM()
+        LIMIT 2;
+       """, nativeQuery = true)
+    List<Question> generateTenRandomQuestions();
+
 }
