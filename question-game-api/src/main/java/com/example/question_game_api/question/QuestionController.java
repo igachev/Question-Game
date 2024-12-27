6efd858c3c82b29493efb570e3860a46e4a34766
igachev <ivophoenix1135@gmail.com>
2024-12-27 16:52:06 +0200

    updated QuestionController
    added DELETE endpoint /questions/:question-id/delete

--- question-game-api/src/main/java/com/example/question_game_api/question/QuestionController.java
+++ question-game-api/src/main/java/com/example/question_game_api/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.example.question_game_api.question;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -59,6 +61,13 @@ public class QuestionController {
         @RequestBody @Valid QuestionRequestDto questionRequestDto
         ) {
         return ResponseEntity.ok(questionService.editQuestion(questionId,questionRequestDto)); 
+    }
+
+    @DeleteMapping("/{question-id}/delete")
+    public ResponseEntity<String> deleteQuestion(
+        @PathVariable(name = "question-id") Integer questionId
+        ) {
+        return new ResponseEntity<String>(questionService.deleteQuestion(questionId),HttpStatus.NO_CONTENT);
     }
     
 }
