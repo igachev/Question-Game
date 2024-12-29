f25d156e894390c6f8b8e7fa6370f91dab19ecd3
igachev <ivophoenix1135@gmail.com>
2024-12-29 17:27:04 +0200

    updated ScoreRepository
    I created method that returns a list of the best score of each player

--- question-game-api/src/main/java/com/example/question_game_api/score/ScoreRepository.java
+++ question-game-api/src/main/java/com/example/question_game_api/score/ScoreRepository.java
@@ -3,11 +3,30 @@ package com.example.question_game_api.score;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.question_game_api.user.User;
 
 public interface ScoreRepository extends JpaRepository<Score,Integer> {
 
     List<Score> findAllByUserOrderByPointsDesc(User existingUser);
-    
+
+    @Query(value = """
+        SELECT s.id,s.user_id,s.played_at,s.points
+        FROM score s
+        INNER JOIN (
+            SELECT user_id, MAX(points) AS max_points
+            FROM score
+            GROUP BY user_id
+        ) AS max_scores
+        ON s.user_id = max_scores.user_id AND s.points = max_scores.max_points
+        WHERE s.id = (
+            SELECT id
+            FROM score
+            WHERE user_id = s.user_id AND points = max_scores.max_points
+            ORDER BY played_at DESC
+            LIMIT 1
+        );
+        """, nativeQuery = true)
+    List<Score> findBestScoreForEachUser();
 }
