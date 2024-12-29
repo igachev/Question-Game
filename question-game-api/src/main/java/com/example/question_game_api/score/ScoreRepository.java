cf6c030ed1082c7daa233367c2b634e787d37f3a
igachev <ivophoenix1135@gmail.com>
2024-12-29 16:00:25 +0200

    updated ScoreRepository
    I added method findAllByUserOrderByPointsDesc().
    This method returns all scores for particular user sorted in descending order by points

--- question-game-api/src/main/java/com/example/question_game_api/score/ScoreRepository.java
+++ question-game-api/src/main/java/com/example/question_game_api/score/ScoreRepository.java
@@ -1,7 +1,13 @@
 package com.example.question_game_api.score;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.question_game_api.user.User;
+
 public interface ScoreRepository extends JpaRepository<Score,Integer> {
+
+    List<Score> findAllByUserOrderByPointsDesc(User existingUser);
     
 }
