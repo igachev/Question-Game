d0768ce3bb53529120f9f65b13c9abbd37db0fe1
igachev <ivophoenix1135@gmail.com>
2024-12-31 18:06:01 +0200

    added header component
    added some styling to the header component

--- question-game-ui/src/app/app.module.ts
+++ question-game-ui/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    HttpClientModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
