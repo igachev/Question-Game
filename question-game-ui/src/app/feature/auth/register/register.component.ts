0c670ef8597880a4ca4e853698fb3edb10893362
igachev <ivophoenix1135@gmail.com>
2025-01-01 17:00:08 +0200

    updated RegisterComponent
    1)I used dependency injection to insert authService and router
    2)created and used registrationSubscription
    3)unsubscribe if needed from registrationSubscription

--- question-game-ui/src/app/feature/auth/register/register.component.ts
+++ question-game-ui/src/app/feature/auth/register/register.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService, RegisterRequestData } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
   private errorMessage!: string;
+  private registrationSubscription!: Subscription;
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+
+  }
 
   onRegister(form: NgForm) {
+    const {email,password,firstName,lastName} = form.value
+    const registerRequestData: RegisterRequestData = {email,password,firstName,lastName}
+    this.registrationSubscription = this.authService.register(registerRequestData)
+    .subscribe({
+      next: (res) => {
+        console.log(res)
+        this.router.navigate(['/'])
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
 
+  ngOnDestroy(): void {
+      if(this.registrationSubscription) {
+        this.registrationSubscription.unsubscribe()
+      }
   }
   
 }
