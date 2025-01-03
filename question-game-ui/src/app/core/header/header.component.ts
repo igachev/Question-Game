955cadf6be347bf117b031be77153f8d0d97f77e
igachev <ivophoenix1135@gmail.com>
2025-01-03 11:35:21 +0200

    updated Header component
    1)created isAuthenticated variable of type boolean
    2)inserted authService using dependency injection
    3)implemented OnInit.Subscribe to the userObject and if there is token return true otherwise false
    4)created onLogout method

--- question-game-ui/src/app/core/header/header.component.ts
+++ question-game-ui/src/app/core/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from 'src/app/feature/auth/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+   isAuthenticated: boolean = false;
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+  this.authService.userObject.subscribe((res) => {
+    this.isAuthenticated = res.token ? true : false
+  })  
+  }
+
+  onLogout(): void {
+    this.authService.userObject.next(new User("","",new Date()))
+    localStorage.removeItem("userData")
+  }
 
 }
