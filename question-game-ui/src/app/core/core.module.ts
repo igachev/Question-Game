e937ec52d54207f304f84439e1cce8115b215720
igachev <ivophoenix1135@gmail.com>
2025-01-06 15:05:12 +0200

    updated Core module
    I forgot to add the interceptor in the Core Module and it wasnt working.I added it now.

--- question-game-ui/src/app/core/core.module.ts
+++ question-game-ui/src/app/core/core.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { AuthInterceptorInterceptor } from './interceptors/auth-interceptor.interceptor';
 
 
 @NgModule({
@@ -16,6 +17,9 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule
   ],
+  providers: [
+      {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptorInterceptor,multi:true}
+    ],
   exports: [
     HeaderComponent,
     FooterComponent
