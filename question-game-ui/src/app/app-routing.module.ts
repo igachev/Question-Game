84560ecedff7120eb66cddd8ae12ce12f7524676
igachev <ivophoenix1135@gmail.com>
2025-01-07 15:05:43 +0200

    updated app-routing.module
    I implemented lazy loading for route /score and module ScoreModule

--- question-game-ui/src/app/app-routing.module.ts
+++ question-game-ui/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'auth',loadChildren:() => import("./feature/auth/auth.module").then((m) => m.AuthModule)},
-  {path:'questions',loadChildren:() => import("./feature/question/question.module").then((m) => m.QuestionModule)}
+  {path:'questions',loadChildren:() => import("./feature/question/question.module").then((m) => m.QuestionModule)},
+  {path:'score',loadChildren:() => import("./feature/score/score.module").then((m) => m.ScoreModule)}
 ];
 
 @NgModule({
