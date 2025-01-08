9e5824aba3899a689c0bb727e462584e93a18a11
igachev <ivophoenix1135@gmail.com>
2025-01-08 15:55:37 +0200

    updated UserScores component
    1) added html,css
    2) injected ScoreService
    3) invoke scoreService.getUserScores() in ngOnInit()

--- question-game-ui/src/app/feature/score/user-scores/user-scores.component.ts
+++ question-game-ui/src/app/feature/score/user-scores/user-scores.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ScoreResponseData, ScoreService } from 'src/app/core/services/score.service';
 
 @Component({
   selector: 'app-user-scores',
   templateUrl: './user-scores.component.html',
   styleUrls: ['./user-scores.component.scss']
 })
-export class UserScoresComponent {
+export class UserScoresComponent implements OnInit,OnDestroy {
+
+  userScores:ScoreResponseData[] = []
+  userScoresSubscription!: Subscription
+
+  constructor(
+    private scoreService: ScoreService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+      this.userScoresSubscription = this.scoreService.getUserScores()
+      .subscribe({
+        next: (data) => {
+          this.userScores = data
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+  };
+
+  ngOnDestroy(): void {
+      if(this.userScoresSubscription) {
+        this.userScoresSubscription.unsubscribe()
+      }
+  }
 
 }
