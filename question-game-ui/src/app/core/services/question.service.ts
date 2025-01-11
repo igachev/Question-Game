cc1e95020d45b765c70e8dce52abb5bcbfca0da1
igachev <ivophoenix1135@gmail.com>
2025-01-11 11:53:49 +0200

    updated QuestionService
    1) added interface QuestionRequestData
    2)created method createQuestion

--- question-game-ui/src/app/core/services/question.service.ts
+++ question-game-ui/src/app/core/services/question.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export interface QuestionResponseData {
   id: number;
@@ -8,6 +10,12 @@ export interface QuestionResponseData {
   answers: string[];
   correctAnswer: string;
   creatorId: string;
+}
+
+export interface QuestionRequestData {
+  question: string;
+  answers: string[];
+  correctAnswer: string;
 }
 
 @Injectable({
@@ -22,6 +30,23 @@ export class QuestionService {
 
   getTenRandomQuestions() {
     return this.http.get<QuestionResponseData[]>("http://localhost:8080/questions")
+  }
+
+  createQuestion(questionRequestData: QuestionRequestData) {
+    return this.http.post<QuestionResponseData>("http://localhost:8080/questions/create",questionRequestData)
+    .pipe(
+          catchError((err) => {
+            let errors: string[] = []
+            if(!err.error || !err.error.errorList) {
+              errors.push("An error occured")
+             return throwError(errors)
+            }
+            else {
+              errors = err.error.errorList
+              return throwError(errors)
+            }
+          })
+        )
   }
 
 }
