6860fc78e13fb40880e9d2a1aba26b87cd29d74a
igachev <ivophoenix1135@gmail.com>
2025-01-11 11:56:53 +0200

    updated CreateQuestion component
    1) injected QuestionService
    2) subscribed to questionService.createQuestion
    3) in case of any server errors store them in property listOfErrors
    4)in the html template I added component Error from module Shared

--- question-game-ui/src/app/feature/question/create-question/create-question.component.ts
+++ question-game-ui/src/app/feature/question/create-question/create-question.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnChanges, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AnswerInputComponent } from '../answer-input/answer-input.component';
+import { QuestionRequestData, QuestionService } from 'src/app/core/services/question.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-question',
   templateUrl: './create-question.component.html',
   styleUrls: ['./create-question.component.scss']
 })
-export class CreateQuestionComponent {
+export class CreateQuestionComponent implements OnDestroy {
 
   numOfPossibleAnswers = [1,2,3,4]
   selectedOption:string = ""
   answers: string[] = []
   @ViewChild(AnswerInputComponent) answerInputComponent!:AnswerInputComponent
+  createQuestionSubscription!: Subscription
+  listOfErrors: string[] = []
+
+  constructor(
+    private questionService: QuestionService
+  ) { }
 
   onCreateQuestion(form: NgForm) {
-    console.log(form)
-    console.log(this.answers)
+    const {question,correctAnswer} = form.value
+    this.answers = this.answers.filter((answer) => answer !== "")
+    const questionRequestData: QuestionRequestData = {question,answers:this.answers,correctAnswer}
+
+    this.createQuestionSubscription = this.questionService.createQuestion(questionRequestData)
+    .subscribe({
+      next: (res) => {
+
+      },
+      error: (err) => {
+        this.listOfErrors = err
+      }
+    });
+    
     form.reset()
     this.selectedOption = ""
     this.answerInputComponent.resetAnswers()
@@ -25,5 +45,11 @@ export class CreateQuestionComponent {
     getAnswers(receivedAnswers: string[]) {
       this.answers = receivedAnswers
       }
+
+    ngOnDestroy(): void {
+        if(this.createQuestionSubscription) {
+          this.createQuestionSubscription.unsubscribe()
+        }
+    }
   
 }
