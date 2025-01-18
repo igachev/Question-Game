f4ab19a1056d8eb453afb687ebc7150d2f84a91d
igachev <ivophoenix1135@gmail.com>
2025-01-18 12:42:00 +0200

    updated Header component
    1) based on user authorization users see different routes in the menu
    2) added css styling to Logout button

--- question-game-ui/src/app/core/header/header.component.ts
+++ question-game-ui/src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { User } from 'src/app/feature/auth/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,20 +11,31 @@ import { User } from 'src/app/feature/auth/user.model';
 export class HeaderComponent implements OnInit {
 
    isAuthenticated: boolean = false;
+   isAdmin: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
 
   }
 
   ngOnInit(): void {
+
   this.authService.userObject.subscribe((res) => {
     this.isAuthenticated = res.token ? true : false
-  })  
+  }) 
+
+  this.authService.userObject.subscribe((res) => {
+    this.isAdmin = res.email === 'admin@abv.bg' ? true : false
+  })
+   
   }
 
   onLogout(): void {
     this.authService.userObject.next(new User("","",new Date()))
     localStorage.removeItem("userData")
+    this.router.navigate(['/'])
   }
 
 }
